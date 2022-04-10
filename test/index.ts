f0ba468538b1256a7599c009a9e849d9dd593cc8
Davy Peter BRAUN <hello@davybraun.com>
2022-04-10 12:33:02 +0200

    test: start testing contract

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,36 @@
-import { expect } from 'chai';
-import { ethers } from 'hardhat';
+import { expect } from 'chai'
+import { ethers } from 'hardhat'
+import { Contract, BigNumber } from 'ethers'
+// import asPromised from 'chai-as-promised'
 
-describe('Greeter', function () {
-  // it('Should return the new greeting once it's changed', async function () {
-  //   const Greeter = await ethers.getContractFactory('Greeter');
-  //   const greeter = await Greeter.deploy('Hello, world!');
-  //   await greeter.deployed();
+const createAndDeploy = async (
+  name: string,
+  symbol: string,
+  baseURI: string,
+  supply: number,
+  price: BigNumber
+): Promise<Contract> => {
+  const Factory = await ethers.getContractFactory('ThirstyThirstySeason01')
+  const contract = await Factory.deploy(
+    name,
+    symbol,
+    baseURI,
+    supply,
+    price
+  )
+  await contract.deployed()
+  return contract
+}
 
-  //   expect(await greeter.greet()).to.equal('Hello, world!');
-
-  //   const setGreetingTx = await greeter.setGreeting('Hola, mundo!');
-
-  //   // wait until the transaction is mined
-  //   await setGreetingTx.wait();
-
-  //   expect(await greeter.greet()).to.equal('Hola, mundo!');
-  // });
+describe('ThirstyThirstySeason01', function () {
+  it('should be deployable with the proper arguments', async () => {
+    const contract = await createAndDeploy(
+      'ThirstyThirsty',
+      'TT',
+      'https://tt.dev',
+      10,
+      BigNumber.from('300000000000000000') // 0.3
+    )
+    expect(contract).to.exist
+  })
 });
