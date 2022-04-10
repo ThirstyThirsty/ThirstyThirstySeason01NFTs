6956709f82d6259f012d07b110ccb7a5621126bb
Davy Peter BRAUN <hello@davybraun.com>
2022-04-10 23:27:06 +0200

    feat: finish v0.5 contract with full test suite

--- contracts/ThirstyThirstySeason01.sol
+++ contracts/ThirstyThirstySeason01.sol
@@ -25,44 +25,58 @@ contract ThirstyThirstySeason01 is ERC721, Ownable, Pausable {
     uint256 public mintPriceInWei; // Top tier = .3ETH, Tier 2 = .08ETH, Goldlist = .05
 
     bytes32 public merkleRoot; // Fill variable before deployment, if used.
-    mapping(address => bool) private goldListClaimed;
+    mapping(address => uint8) private _mintsPerUser;
 
     constructor(
         string memory _name,
         string memory _symbol,
         string memory _metadataURI,
         uint256 _maxSupply,
-        uint256 _mintPriceInWei
+        uint256 _mintPriceInWei,
+        bytes32 _merkleRoot
     ) ERC721(_name, _symbol) {
         maxSupply = _maxSupply;
         mintPriceInWei = _mintPriceInWei;
+        merkleRoot = _merkleRoot;
         _metadataBaseURI = _metadataURI;
 
         _nextTokenId.increment();
     }
 
-    function mint(bytes32[] calldata _merkleProof) public payable {
-        // If merkle root is set, we're filtering attempts through goldlist membership.
-        if (merkleRoot != 0) {
-            require(!goldListClaimed[msg.sender], "Address has already claimed");
-            bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
-            require(MerkleProof.verify(_merkleProof, merkleRoot, leaf), "Address not in goldlist");
-        }
-
+    function mint() public payable {
         uint256 mintIndex = _nextTokenId.current();
         // Avoid using >= and <= for performance reason.
-        require((mintIndex < maxSupply) || (mintIndex == maxSupply), "Sold out.");
+        require((mintIndex < maxSupply) || (mintIndex == maxSupply), "Sold out");
         require((msg.value > mintPriceInWei) || (msg.value == mintPriceInWei), "Not enough fund");
-        if (merkleRoot != 0) {
-            goldListClaimed[msg.sender] = true;
-        }
+        // Max mint is 6, not stored in storage variable for performance reasons
+        require((_mintsPerUser[msg.sender] < 6), "No more mint for user");
 
+        _mintsPerUser[msg.sender] += 1;
         _nextTokenId.increment();
         _safeMint(msg.sender, mintIndex);
     }
 
-    function tokensMinted() public view returns (uint256) {
+    function mintGold(bytes32[] calldata _merkleProof) public payable {
+        if (merkleRoot != 0) {
+            require(_mintsPerUser[msg.sender] == 0, "Address has already claimed");
+            bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+            require(MerkleProof.verify(_merkleProof, merkleRoot, leaf), "Address not in goldlist");
+        }
+        uint256 mintIndex = _nextTokenId.current();
+        require((mintIndex < maxSupply) || (mintIndex == maxSupply), "Sold out");
+        require((msg.value > mintPriceInWei) || (msg.value == mintPriceInWei), "Not enough fund");
+
+        _mintsPerUser[msg.sender] += 1;
+        _nextTokenId.increment();
+        _safeMint(msg.sender, mintIndex);
+    }
+
+    function nextTokenID() public view returns (uint256) {
         return _nextTokenId.current();
+    }
+
+    function numOfMints() public view returns (uint256) {
+        return _nextTokenId.current() - 1;
     }
 
     function pause() public onlyOwner {
