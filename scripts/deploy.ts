b79bfcd242548658cba43edcc4c4f31bb05fff79
Davy Peter BRAUN <hello@davybraun.com>
2022-05-23 23:17:12 +0200

    feat: update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,7 +4,20 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import hre, { ethers } from 'hardhat';
-import { openSeaProxyRegistryAddress } from '../utils/constants'
+import {
+  OS_PROXY_ADDR,
+  PRICE_CELLAR,
+  PRICE_TABLE,
+  PRICE_TABLE_GOLD,
+  TIER_CELLAR_ID,
+  TIER_TABLE_ID,
+  TIER_TABLE_GOLD_ID,
+  TIER_FRENS_ID,
+  MERKLE_ROOT,
+  NAME,
+  SYMBOL,
+  METADATA_BASE_URI
+} from '../utils/constants'
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -12,56 +25,54 @@ async function main() {
   //
   // If this script is run directly using `node` you may want to call compile
   // manually to make sure everything is compiled
-  await hre.run('compile');
+  await hre.run('compile')
 
-  const Factory = await ethers.getContractFactory('ThirstyThirstySeason01');
-  const tierCellar = await Factory.deploy(
-    'Thirsty Thirsty Season 1 - "Cellar"',
-    'TT',
-    '',
-    openSeaProxyRegistryAddress,
-    270,
-    ethers.utils.parseEther('0.3'),
-    ethers.utils.hexZeroPad('0x00', 32)
-  );
-  const tierTable = await Factory.deploy(
-    'Thirsty Thirsty Season 1 - "Table"',
-    'TT',
-    '',
-    openSeaProxyRegistryAddress,
-    418,
-    ethers.utils.parseEther('0.17'),
-    ethers.utils.hexZeroPad('0x00', 32)
-  );
-  const tierTableGold = await Factory.deploy(
-    'Thirsty Thirsty Season 1 - "Table" Goldlist',
-    'TT',
-    '',
-    openSeaProxyRegistryAddress,
-    100,
-    ethers.utils.parseEther('0.07'),
-    '0x31ee47f7fbec35a75a75ee71d0d72c71970c5cc8ecf2f7f5ec4e39a5f40adade', // CHANGE WITH MOST RECENT MERKLE ROOT
-    // ethers.utils.hexZeroPad('0x00', 32)
-  );
-  const tierFrens = await Factory.deploy(
-    'Thirsty Thirsty Season 1 - "Frens & Fams"',
-    'TT',
-    '',
-    openSeaProxyRegistryAddress,
-    100,
-    ethers.utils.parseEther('0'),
-    ethers.utils.hexZeroPad('0x00', 32)
-  );
+  const Factory = await ethers.getContractFactory('ThirstyThirstySeason01')
 
-  await tierCellar.deployed();
-  await tierTable.deployed();
-  await tierTableGold.deployed();
-  await tierFrens.deployed();
+  const tierCellar = {
+    id: TIER_CELLAR_ID,
+    minted: 0,
+    supply: 270,
+    priceInWei: PRICE_CELLAR
+  }
+  const tierTable = {
+    id: TIER_TABLE_ID,
+    minted: 0,
+    supply: 518,
+    priceInWei: PRICE_TABLE
+  }
+  const tierTableGold = {
+    id: TIER_TABLE_GOLD_ID,
+    minted: 0,
+    supply: 100,
+    priceInWei: PRICE_TABLE_GOLD
+  }
+  const tierFriends = {
+    id: TIER_FRENS_ID,
+    minted: 0,
+    supply: 50,
+    priceInWei: ethers.BigNumber.from(0)
+  }
 
-  console.log('"Cellar" deployed to:', tierCellar.address);
-  console.log('"Table" deployed to:', tierTable.address);
-  console.log('"Table Gold" deployed to:', tierTableGold.address);
-  console.log('"Frens & Fam" deployed to:', tierFrens.address);
+  const contract = await Factory.deploy(
+    NAME,
+    SYMBOL,
+    METADATA_BASE_URI,
+    false, // isMintStarted => false, so contract launches with goldlist minting only
+    OS_PROXY_ADDR,
+    tierCellar,
+    tierTable,
+    tierTableGold,
+    tierFriends,
+    MERKLE_ROOT
+  )
+
+  await contract.deployed()
+
+
+  console.log("\n")
+  console.log('`Thirsty Thirsty` contract deployed to:', contract.address)
+  console.log("\n")
 }
 
 // We recommend this pattern to be able to use async/await everywhere
